413129b1c2da1011a298b06e79ce96be962d4f3b
chandra-mouli1652 <chandubynapalepu21@gmail.com>
2023-12-05 08:12:04 +0000

    music playlist tests solution

---
+++ src/components/MusicItem/styledComponents.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+
+export const MusicListItem = styled.li`
+  display: flex;
+`
+
+export const TrackInfoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 500px;
+`
+
+export const MusicTrackImage = styled.img`
+  height: 90px;
+  width: 140px;
+`
+export const NameGenreContainer = styled.div``
+
+export const Name = styled.p`
+  font-size: 18px;
+`
+
+export const Genre = styled.p`
+  font-size: 18px;
+`
+
+export const DurationDeleteContainer = styled.div`
+  display: flex;
+`
+
+export const Duration = styled.p`
+  font-family: 'roboto';
+`
+export const DeleteButton = styled.button`
+  background-color: transparent;
+`
---
+++ src/components/MusicPlayList/styledComponents.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+
+export const BgContainer = styled.div`
+  background-color: #152850;
+  min-height: 100vh;
+`
+
+export const ArtistBg = styled.div`
+  background-image: url('https://assets.ccbp.in/frontend/react-js/music-playlist/music-playlist-Edsheeran-bg.png');
+`
+
+export const ArtistName = styled.h1`
+  font-size: 42px;
+`
+export const ArtistInfo = styled.p`
+  font-family: 'Roboto';
+`
+export const PlayListContainer = styled.div`
+  display: flex;
+`
+export const PlayListHeading = styled.h1`
+  font-size: 32px;
+`
+export const SearchInput = styled.input`
+  height: 30px;
+  width: 18%;
+`
+export const MusicList = styled.ul`
+  padding: 0;
+`
+export const EmptyContainer = styled.div`
+  display: flex;
+`
+export const NoSongsText = styled.p`
+  font-size: 32px;
+`
